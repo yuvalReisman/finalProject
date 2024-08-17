c0a91a571881cedff1f7df01d09a09417da187a2
yuvalReisman <yuvalreis21@gmail.com>
2024-08-17 14:14:14 +0300

    added options

--- src/test/java/testsTopMarket/BaseTest.java
+++ src/test/java/testsTopMarket/BaseTest.java
@@ -2,6 +2,7 @@ package testsTopMarket;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -12,7 +13,6 @@ import pagesTopMarket.ItemPage;
 import pagesTopMarket.ItemsPage;
 import pagesTopMarket.NavigationPage;
 
-
 public class BaseTest {
 
 	WebDriver driver;
@@ -22,24 +22,26 @@ public class BaseTest {
 	CartPage cartpage;
 	ItemsPage itemspage;
 	CheckoutPage checkoutpage;
-	
+
 	@BeforeClass
 	public void setUp() {
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless", "--window-size=1920,1080");
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://www.topmarket.co.il/en/");
 	}
-	
+
 	@BeforeClass
 	public void setUpForPages() {
-		homepage=new HomePage(driver);
-		itempage=new ItemPage(driver);
-		navigationpage=new NavigationPage(driver);
-		cartpage=new CartPage(driver);
-		itemspage=new ItemsPage(driver);
-		checkoutpage=new CheckoutPage(driver);
+		homepage = new HomePage(driver);
+		itempage = new ItemPage(driver);
+		navigationpage = new NavigationPage(driver);
+		cartpage = new CartPage(driver);
+		itemspage = new ItemsPage(driver);
+		checkoutpage = new CheckoutPage(driver);
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
